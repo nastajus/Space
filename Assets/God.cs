1e5566a8d79796f2080790f943148cd36f636b5a
Victor <victor.nguyen@hotmail.com>
2015-09-19 18:55:04 -0400

    IT works again.....
    Left and Right work when button is held down.

--- Assets/God.cs
+++ Assets/God.cs
@@ -3,6 +3,32 @@ using System.Collections;
 
 public class God : MonoBehaviour {
 
+	bool leftDown = false;
+	bool rightDown = false;
+
+	public void Left(int status){
+		switch (status) {//0=Off 1=On
+		case 0: leftDown = false;break;
+		case 1: leftDown = true;break;
+		}
+	}
+	public void Right(int status){
+		switch (status) {
+		case 0: rightDown = false;break;
+		case 1: rightDown = true;break;
+		}
+	}
+
+
+	public void Update(){
+		if (leftDown) {
+			LeftPressed();
+		}
+		if (rightDown) {
+			RightPressed();
+		}
+	}
+
     public void LeftPressed()
     {
         BroadcastMessage("Move", -1);
