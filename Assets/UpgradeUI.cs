cf7f2803632900aa4e04a00c827e5e6bae8528cd
Victor <victor.nguyen@hotmail.com>
2015-09-20 00:35:20 -0400

    Prototype Menu Upgrade System

---
+++ Assets/UpgradeUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeUI : MonoBehaviour {
+
+    private RectTransform myt;
+    private float initialY;
+    private bool isOpen;
+
+	// Use this for initialization
+	void Start () {
+        myt = GetComponent<RectTransform>();
+        initialY = myt.anchoredPosition.y;
+        isOpen = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isOpen) {
+            if (myt.anchoredPosition.y < 0)
+                myt.anchoredPosition = new Vector2(0.0f, myt.anchoredPosition.y+1080*Time.deltaTime);
+        }
+        else
+
+            if (myt.anchoredPosition.y > initialY)
+             myt.anchoredPosition = new Vector2(0.0f, myt.anchoredPosition.y - 1080 * Time.deltaTime);
+            else
+            {
+                myt.anchoredPosition = new Vector2(0.0f, initialY);
+            }
+    }
+       
+    
+    
+    public void Toggle()
+    {
+        isOpen = !isOpen;
+    }
+    
+}
